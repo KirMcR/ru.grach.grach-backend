76ac34ce4c8515a7900c928c6d414d3c5039ce41
KirMcR <Kirillmcarov@gmail.com>
2022-09-19 18:59:25 +0300

    I justr tried to do commits

--- src/test/kotlin/ru/grach/ApplicationTest.kt
+++ src/test/kotlin/ru/grach/ApplicationTest.kt
@@ -9,17 +9,18 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import org.junit.Test
 import ru.grach.plugins.*
 
-//class ApplicationTest {
-//    @Test
-//    fun testRoot() = testApplication {
-//        application {
-//            configureRouting()
-//        }
-//        client.get("/").apply {
-//            assertEquals(HttpStatusCode.OK, status)
-//            assertEquals("Hello World!", bodyAsText())
-//        }
-//    }
-//}
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
